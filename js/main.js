a4eea2f06ed346379c8f1ef08048f03d200bfb87
Alessandro Nocella <sberz666@gmail.com>
2016-05-28 08:17:12 +0200

    sistemato responsive

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ $(document).ready(function(){
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
             if (target.length) {
-                $('html,body').animate({
-                  scrollTop: target.offset().top
+                $('html,body').animate({					
+                  scrollTop: 0
                 }, 1000);
                 return false;
               }
